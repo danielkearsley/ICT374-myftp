36b5ac339820dc5c9f0689c01fc7d68cf4cf7483
Clem Davies <clem.wdavies@gmail.com>
2017-11-03 13:19:37 +0800

    fixed stream.c in server

--- myftpd/stream.c
+++ myftpd/stream.c
@@ -1,128 +1,91 @@
-/*
- *  stream.c  -	(Topic 11, HX 22/5/1995)
- *	 	routines for stream read and write.
+/**
+ * AUTHOR: Clem Davies, Daniel Kearsley
+ * DATE: 3/11/17
+ * FILENAME: stream.h
+ * DESCRIPTION: Functions used to read and write data on a socket.
  */
-
-#include  <sys/types.h>
-#include  <netinet/in.h> /* struct sockaddr_in, htons(), htonl(), */
-#include  "stream.h"
-
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include <stdio.h>
+#include "stream.h"
 
-
-
-
-int writen(int sd, char *buf, int nbytes)
-{
-    short data_size = nbytes;     /* short must be two bytes long */
-    int n, nw;
-
-    if (nbytes > MAX_BLOCK_SIZE)
-         return (-3);    /* too many bytes to send in one go */
-
-    /* send the data size */
-    data_size = htons(data_size);
-    if (write(sd, (char *) &data_size, 1) != 1) return (-1);
-    if (write(sd, (char *) (&data_size)+1, 1) != 1) return (-1);
-
-    /* send nbytes */
-    for (n=0; n<nbytes; n += nw) {
-         if ((nw = write(sd, buf+n, nbytes-n)) <= 0)
-             return (nw);    /* write error */
-    }
-    return (n);
-}
-
-
-int readn(int sd, char *buf, int bufsize)
-{
-    short data_size;    /* sizeof (short) must be 2 */
-    int n, nr, len;
-
-    /* check buffer size len */
-    if (bufsize < MAX_BLOCK_SIZE)
-         return (-3);     /* buffer too small */
-
-    /* get the size of data sent to me */
-    if (read(sd, (char *) &data_size, 1) != 1) return (-1);
-    if (read(sd, (char *) (&data_size)+1, 1) != 1) return (-1);
-    len = (int) ntohs(data_size);  /* convert to host byte order */
-
-    /* read len number of bytes to buf */
-    for (n=0; n < len; n += nr) {
-        if ((nr = read(sd, buf+n, len-n)) <= 0)
-            return (nr);       /* error in reading */
-    }
-    return (len);
-}
-
-
-
-
+/*
+ * Loops over a write to socket until entire nbytes is written from buf.
+ */
 int write_nbytes(int sd, char *buf, int nbytes)
 {
 	int nw = 0;
 	int n = 0;
 	for (n=0; n < nbytes; n += nw) {
 		if ((nw = write(sd, buf+n, nbytes-n)) <= 0)
-			return (nw);    /* write error */
+			return (nw); /* write error */
 	}
   return n;
 }
 
-
+/*
+ * Loops over a read from socket until entire nbytes read into buf.
+ */
 int read_nbytes(int sd, char *buf, int nbytes)
 {
 	int nr = 1;
 	int n = 0;
 	for (n=0; (n < nbytes) && (nr > 0); n += nr) {
 		if ((nr = read(sd, buf+n, nbytes-n)) < 0){
-			return (nr);       /* error in reading */
+			return (nr); /* read error */
 		}
 	}
 	return (n);
 }
 
-
+/*
+ * Writes a one byte char on the socket.
+ */
 int write_code(int sd, char code)
 {
-
-	/* send the code */
+	/* write 1 byte code to socket */
 	if (write(sd, (char*)&code, 1) != 1) return (-1);
 
-	//return success
 	return 1;
 }
 
+/*
+ * Reads a one byte char off the socket.
+ */
 int read_code(int sd, char* code)
 {
 	char data;
 
-	// read 1 byte code from socket
-	if(read(sd,(char *) &data,1) != 1)
-		return -1; // read failed
+	/* read 1 byte code from socket */
+	if(read(sd,(char *) &data,1) != 1) return -1;
+
 	*code = data;
-	// return success;
+
 	return 1;
 }
 
-
+/*
+ * Writes a two byte integer on the socket.
+ */
 int write_twobytelength(int sd, int length)
 {
 	short data = length;
-	data = htons(data);  //convert to network byte order
+	data = htons(data);  /* convert to network byte order */
 
 	if (write(sd,&data, 2) != 2) return (-1);
 
 	return 1;
 }
 
+/*
+ * Reads a two byte integer off the socket.
+ */
 int read_twobytelength(int sd, int *length)
 {
 	short data = 0;
 
   if (read(sd, &data, 2) != 2) return (-1);
+
   short conv = ntohs(data); /* convert to host byte order */
   int t = (int)conv;
   *length = t;
@@ -130,24 +93,29 @@ int read_twobytelength(int sd, int *length)
 	return 1;
 }
 
+/*
+ * Writes a four byte integer on the socket.
+ */
 int write_fourbytelength(int sd, int length)
 {
-	int data = htonl(length); //convert to network byte order
+	int data = htonl(length); /* convert to network byte order */
 
 	if (write(sd,&data, 4) != 4) return (-1);
 
 	return 1;
 }
 
+/*
+ * Reads a four byte integer off the socket.
+ */
 int read_fourbytelength(int sd, int *length)
 {
 	int data = 0;
 
   if (read(sd, &data, 4) != 4) return (-1);
+
   int conv = ntohl(data); /* convert to host byte order */
   *length = conv;
 
 	return 1;
 }
-
-
--- myftpd/stream.h
+++ myftpd/stream.h
@@ -1,75 +1,64 @@
-/*
- *  stream.h  - Topic 11, HX 22/5/1995)
- *	 	head file for stream read and stream write.
+/**
+ * AUTHOR: Clem Davies, Daniel Kearsley
+ * DATE: 3/11/17
+ * FILENAME: stream.h
+ * DESCRIPTION: Defines functions used to read and write data on a socket.
  */
 
-
-#define MAX_BLOCK_SIZE (1024*5)    /* maximum size of any piece of */
-                                   /* data that can be sent by client */
-
-
 /*
- * purpose:  write "nbytes" bytes from "buf" to "sd".
- * pre:      1) nbytes <= MAX_BLOCK_SIZE,
- * post:     1) nbytes bytes from buf written to sd;
- *           2) return value = nbytes : number ofbytes written
- *                           = -3     : too many bytes to send
- *                           otherwise: write error
+ * Writes a stream of bytes on socket sd from buf
+ * returns:
+ * >0 : number of bytes written
+ * otherwise : write error
  */
-int writen(int sd, char *buf, int nbytes);
-
-/*
- * purpose:  read a stream of bytes from "sd" to "buf".
- * pre:      1) size of buf bufsize >= MAX_BLOCK_SIZE,
- * post:     1) buf contains the byte stream;
- *           2) return value > 0   : number ofbytes read
- *                           = 0   : connection closed
- *                           = -1  : read error
- *                           = -2  : protocol error
- *                           = -3  : buffer too small
- */
-int readn(int sd, char *buf, int bufsize);
-
-
-
 int write_nbytes(int sd, char *buf, int nbytes);
 
+/*
+ * Reads a stream of bytes on socket sd to buf
+ * returns:
+ * >0 : number of bytes read
+ * otherwise : read error
+ */
 int read_nbytes(int sd, char *buf, int nbytes);
 
-
-
-/* Writes a one byte char from opcode to socket sd.
+/*
+ * Writes a one byte char from opcode to socket sd.
  * return: -1 : write failed
  *					1 : write success
  */
 int write_code(int sd, char code);
 
-/* Reads a one byte char from socket sd to opcode.
+/*
+ * Reads a one byte char from socket sd to opcode.
  * return: -1 : read failed
  *					1 : read success
  */
 int read_code(int sd,char *code);
 
-
-
-/* Writes a two byte integer from length to socket sd.
+/*
+ * Writes a two byte integer from length to socket sd.
  * return: -1 : write failed
  *					1 : write success
  */
 int write_twobytelength(int sd, int length);
 
+/*
+ * Reads a two byte integer from socket sd to length.
+ * return: -1 : read failed
+ *					1 : read success
+ */
 int read_twobytelength(int sd, int *length);
 
-
-/* Writes a four byte integer from length to socket sd.
+/*
+ * Writes a four byte integer from length to socket sd.
  * return: -1 : write failed
  *					1 : write success
  */
 int write_fourbytelength(int sd, int length);
 
+/*
+ * Reads a four byte integer from socket sd to length.
+ * return: -1 : read failed
+ *					1 : read success
+ */
 int read_fourbytelength(int sd, int *length);
-
-
-
-
-
